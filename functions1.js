866cde75be3f9a697af9939f135d8213d3d993be
Drew Conly <atconly@gmail.com>
2016-07-09 11:11:53 -0600

    complete syntaxValidator

--- functions1.js
+++ functions1.js
@@ -2,38 +2,69 @@
 // returns boolean value
 // parens, square-brackets, & curly-brackets are properly nested and closed
 
+// var syntaxValidator = function(functionString) {
+//   var leftParenCounter = 0;
+//   var rightParenCounter = 0;
+//   var leftSquareCounter = 0;
+//   var rightSquareCounter = 0;
+//   var leftCurlyCounter = 0;
+//   var rightCurlyCounter = 0;
+//   for (i = 0; i < functionString.length; i++) {
+//     if (functionString[i] == '(') {
+//       leftParenCounter++;
+//     } else if (functionString[i] == ')') {
+//       rightParenCounter++;
+//     } else if (functionString[i] == '[') {
+//       leftSquareCounter++;
+//     } else if (functionString[i] == ']') {
+//       rightSquareCounter++;
+//     } else if (functionString[i] == '{') {
+//       leftCurlyCounter++;
+//     } else if (functionString[i] == '}') {
+//       rightCurlyCounter++;
+//     }
+//   }
+//   ((leftParenCounter === rightParenCounter) && (leftSquareCounter === rightSquareCounter) && (leftCurlyCounter === rightCurlyCounter) ? console.log(true) : console.log(false) )
+// }
+
 var syntaxValidator = function(functionString) {
-  var leftParenCounter = 0;
-  var rightParenCounter = 0;
-  var leftSquareCounter = 0;
-  var rightSquareCounter = 0;
-  var leftCurlyCounter = 0;
-  var rightCurlyCounter = 0;
+  var openStack = [];
+  var closeStack = [];
   for (i = 0; i < functionString.length; i++) {
-    if (functionString[i] == '(') {
-      leftParenCounter++;
+    if (functionString[i] == '(' || functionString[i] == '[' || functionString[i] == '{') {
+      openStack.push(functionString[i]);
     } else if (functionString[i] == ')') {
-      rightParenCounter++;
-    } else if (functionString[i] == '[') {
-      leftSquareCounter++;
+      if (openStack.pop() === '(') {
+        continue
+      } else {
+        console.log(false);
+        return false;
+      }
     } else if (functionString[i] == ']') {
-      rightSquareCounter++;
-    } else if (functionString[i] == '{') {
-      leftCurlyCounter++;
+      if (openStack.pop() === '[') {
+        continue
+      } else {
+        console.log(false);
+        return false;
+      }
     } else if (functionString[i] == '}') {
-      rightCurlyCounter++;
+      if (openStack.pop() === '{') {
+        continue
+      } else {
+        console.log(false);
+        return false;
+      }
     }
   }
-  ((leftParenCounter === rightParenCounter) && (leftSquareCounter === rightSquareCounter) && (leftCurlyCounter === rightCurlyCounter) ? console.log(true) : console.log(false) )
-  // console.log('leftParenCounter: ' + leftParenCounter);
-  // console.log('rightParenCounter: ' + rightParenCounter);
-  // console.log('leftSquareCounter: ' + leftSquareCounter);
-  // console.log('rightSquareCounter: ' + rightSquareCounter);
-  // console.log('leftCurlyCounter: ' + leftCurlyCounter);
-  // console.log('rightCurlyCounter: ' + rightCurlyCounter);
+  console.log(true);
+  return true;
 }
 
-
 var testString1 = "function htmlDoc(title, bodyContent) {return tag('html', [tag('head', [tag('title', [title])]), tag('body', bodyContent)]);}"
-
+var testString2 = "function htmlDoc(title, bodyContent) {return tag('html', [tag('head', [tag('title', [title])), tag('body', bodyContent)]);}"
+var testString3 = "function htmlDoc(title, bodyContent) {return tag('html', [tag 'head', [tag('title', [title])]), tag('body', bodyContent)]);}"
+var testString4 = "function htmlDoc(title, bodyContent) {return tag('html', [tag('head', [tag('title', [title]])), tag('body', bodyContent)]);}"
 syntaxValidator(testString1);
+syntaxValidator(testString2);
+syntaxValidator(testString3);
+syntaxValidator(testString4);
